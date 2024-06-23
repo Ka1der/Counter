79394155450bfb7c6414d96d1ce33597484f6652
Kaider <kronoz@yandex.ru>
2024-06-23 23:19:38 +0300

    Проект создан

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import Foundation
+
+private var counter = 0
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var munisButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var historyFeild: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelCounter.text = "Значение счетчика: \(counter)"
+        historyFeild.text = ""
+        historyFeild.isEditable = false
+        labelCounter.font = UIFont.boldSystemFont(ofSize: 15)
     }
-
-
+    func updateTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let currentDate = dateFormatter.string(from: Date())
+        return currentDate
+    }
+    @IBAction func buttonDidTapPlusCounter(_ sender: Any) {
+        counter += 1
+        labelCounter.text = "Значение счетчика: \(counter)"
+        
+        let newCount = "\(updateTime()) Значение изменено на +1\n"
+        historyFeild.text += newCount
+        
+        let scrollHistoryField = NSMakeRange(historyFeild.text.count - 1, 1)
+        historyFeild.scrollRangeToVisible(scrollHistoryField)
+    }
+    @IBAction func buttonDidTapMinusCounter(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            labelCounter.text = "Значение счетчика: \(counter)"
+            
+            let newCount = "\(updateTime()) Значение изменено на -1\n"
+            historyFeild.text += newCount
+            
+            let scrollHistoryField = NSMakeRange(historyFeild.text.count - 1, 1)
+            historyFeild.scrollRangeToVisible(scrollHistoryField)
+        } else {
+            if counter <= 0 {
+                counter = 0
+                
+            let negativeCount = "\(updateTime()) Попытка уменьшения значения счетчика ниже 0\n"
+            historyFeild.text += negativeCount
+                
+            let scrollHistoryField = NSMakeRange(historyFeild.text.count - 1, 1)
+            historyFeild.scrollRangeToVisible(scrollHistoryField)
+                }
+            }
+        }
+    @IBAction func buttotDidTapResetCounter(_ sender: Any) {
+        counter = 0
+        labelCounter.text = "Значение счетчика: \(counter)"
+        
+        let newCount = "\(updateTime()) Значение сброшено\n"
+        historyFeild.text += newCount
+        
+        let scrollHistoryField = NSMakeRange(historyFeild.text.count - 1, 1)
+        historyFeild.scrollRangeToVisible(scrollHistoryField)
+    }
 }
 
